db21c2606332912b75975f95f04e23cdf4c1c464
Jason Milen <jason.milen@opengear.com>
2022-09-01 12:09:42 +1000

    Move the source file contents into the main edit script. I now see no need to separate them at this point.

--- encryption-PoC/edit-encrypted-file.sh
+++ encryption-PoC/edit-encrypted-file.sh
@@ -16,6 +16,20 @@ function clean_up() {
 	fi
 }
 
+function encrypt_file() {
+	infile=$1
+	outfile=$2
+	password=$3
+	echo "${password}" | openssl aes-256-cbc -pbkdf2 -iter 10000 -salt -in ${infile} -out ${outfile} -pass stdin
+}
+
+function decrypt_file() {
+	infile=$1
+	outfile=$2
+	password=$3
+	echo "${password}" | openssl aes-256-cbc -d -pbkdf2 -iter 10000 -in ${infile} -out ${outfile} -pass stdin
+}
+
 if [[ -z ${_file// } ]]; then
 	echo "No file specified. Nothing to do."
 	exit 1
