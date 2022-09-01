34cbc37e9f87638a96219d3e4c300f1b44b613b1
Jason Milen <jason.milen@opengear.com>
2022-09-01 10:54:10 +1000

    Add PoC for bash encryption/decryption and then also decryption in python

---
+++ encryption-PoC/pyread.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+'''
+This is just a proof of concept to read a file encrypted by openssl aes-256-cbc
+from the bash command line.
+
+The reason for this is that the file needs to be decrypted, edited and re-encrypted
+in bash. The file is then decrypted and used python script.
+'''
+
+import binascii
+import base64
+import hashlib
+from getpass import getpass
+from Crypto.Cipher import AES
+
+infile = "./encrypted"
+password = getpass()
+pbkdf2iterations=10000
+
+passwordbytes = password.encode('utf-8')
+
+with open(infile, 'rb') as f:
+    openssloutputbytes = f.read()
+
+salt = openssloutputbytes[8:16]
+
+derivedkey = hashlib.pbkdf2_hmac('sha256', passwordbytes, salt, pbkdf2iterations, 48)
+
+key = derivedkey[0:32]
+iv = derivedkey[32:48]
+
+ciphertext = openssloutputbytes[16:]
+
+decryptor = AES.new(key, AES.MODE_CBC, iv)
+plaintext = decryptor.decrypt(ciphertext)
+
+plaintext = plaintext[:-plaintext[-1]]
+print(plaintext.decode())
