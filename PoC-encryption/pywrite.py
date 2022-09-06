6f9cd3c9cb8d195a5c48ca67f4510f7beb1d3851
Jason Milen <jason.milen@opengear.com>
2022-09-06 17:41:09 +1000

    Change pywrite.py PoC to take bytes rather than a string as the data to be encrypted and improve comments.

--- PoC-encryption/pywrite.py
+++ PoC-encryption/pywrite.py
@@ -11,36 +11,60 @@ import hashlib
 from Crypto.Cipher import AES
 from Crypto import Random
 
-password = 'some password'
-plaintext = "some \nrandom plaintext\ndata\n"
-
 iterations = 10000
 bs = AES.block_size
 
 
-def _pad(s):
+def _pad(s: bytes) -> bytes:
     '''
     The length of the data to be encrypted needs to be a multiple of AES.block_size.
 
     NOTE: the padding bytes are each set to the actual number of pad bytes.
-    I think the requirement is that only the last byte needs to be set to that
-    but it's easier to set them all to that.
+    The requirement is that only the last byte needs to be set to that
+    but it's easier to just write them all with the same value.
     '''
+    # pad_len can never be 0 as the number of padding bytes always needs to be written.
+    # The calculation of pad_len below always produces a result from '1 to bs' inclusive
     pad_len = bs - (len(s) % bs)
-    return s + ((pad_len) * chr(pad_len))
+    return s + (pad_len.to_bytes(1, byteorder='big') * pad_len)
 
 
-salt = Random.new().read(AES.block_size - len('Salted__'))
-derived_key = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, iterations, 48)
-key = derived_key[0:32]
-iv = derived_key[32:48]
+def _unpad(s: bytes) -> bytes:
+    ''' NOTE: not used but put here as a demonstration of how to do this
+    '''
+    pad_bytes_count = s[-1]
+    return s[:-pad_bytes_count]
 
 
-plaintext_bytes = _pad(plaintext).encode()
-encryptor = AES.new(key, AES.MODE_CBC, iv)
+def encrypt(plaintext_bytes: bytes, password: str) -> bytes:
+    '''
+    Returns bytes that look like:
+        Salted__XXXXXXXXDDDDDDDD...P
 
-ciphertext = encryptor.encrypt(plaintext_bytes)
+    where:
+        XXXXXXXX = salt
+        DDDDDDDD... = encrypted data
+        P = number of padding bytes
+    '''
+    salt = Random.new().read(AES.block_size - len('Salted__'))
+    derived_key = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, iterations, 48)
+    key = derived_key[0:32]
+    iv = derived_key[32:48]
 
+
+    plaintext_bytes = _pad(plaintext_bytes)
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+
+    ciphertext = encryptor.encrypt(plaintext_bytes)
+    return 'Salted__'.encode() + salt + ciphertext
+
+
+password = 'some password'
+plaintext = "0123456789abcdef0"
+
+ciphertext = encrypt(plaintext.encode(), password)
 with open("./pyencrypted", 'wb') as f:
-    f.write('Salted__'.encode() + salt)
     f.write(ciphertext)
+
+test_string = "some string"
+assert _unpad(_pad(test_string.encode())).decode() == test_string
